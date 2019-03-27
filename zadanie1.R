2a96effc354258474ebe729a432f616f79d3ae1d
Nik-Dan <48888342+Nik-Dan@users.noreply.github.com>
2019-03-27 16:37:23 +0300

    Add files via upload

--- zadanie1.R
+++ zadanie1.R
@@ -3,6 +3,8 @@ library(rnoaa)
 
 #install.packages(c("tidyverse","rnoaa"))
 
+
+
 #station_data = ghcnd_stations() #Может занять несколько минут лучше выполнить один раз в месте с хорошим интернетом и сохранить результат
 
 #write.csv(station_data,"station_data.csv")
@@ -22,13 +24,23 @@ city_around = meteo_nearby_stations(lat_lon_df = city, station_data = station_da
 
 city_around[[1]][,1:3]
 
-city_id = city_around[[1]][["id"]][1]
+city_id = city_around[[1]][["id"]]
+#[1]
 
 #tula_around это список единственным элементом которого является таблица, содержащая идентификаторы метеостанций отсортированных по их 
 # удалленности от Тулы, очевидно что первым элементом таблицы будет идентификатор метеостанции Тулы, его то мы и попытаемся получить
 #tula_id = tula_around[["TULA"]][["id"]][1]
 #Для получения всех данных с метеостанции, зная ее идентификатор, используйте след. команду
-all_city_data = meteo_tidy_ghcnd(stationid = city_id,date_min = "2008-01-01",date_max = "2013-12-31")
+
+for (i in 1:length(city_id))
+{
+  city_data = meteo_tidy_ghcnd(stationid = city_id[i],date_min = "2008-01-01",date_max = "2013-12-31") 
+  r<-city_data[,c("id","date","tavg")]
+  if (i==1) all_city_data<-r else all_city_data<-rbind(all_city_data,r)
+}
+
+all_city_data$tavg<-as.double(all_city_data$tavg)/10
+
 head(all_city_data$date)
 tail(all_city_data$date)
 
@@ -44,13 +56,28 @@ mm<-str_sub(string = all_city_data2$date, start = 6, end = 7)
 
 all_city_data2$mm<-mm
 
+year<-str_sub(string = all_city_data2$date, start = 1, end = 4)
+
+all_city_data2$year<-year
+
+
+
+all_city_data3 = all_city_data2%>%group_by(year, mm, id)%>% summarise(total = sum(tavg))
+
+all_city_data3 = all_city_data2%>%group_by(mm,)%>% summarise(total = min(tavg))
+##s<-
+
 #df1<-data.frame(mid=mm,t_more_5=t_more_5)
 
 ss <- split.data.frame(all_city_data2,all_city_data2$mm)
 
 tavg<-c()
-for (i in 1:12) tavg<-c(tavg,sum(ss[i][[1]]$tavg))
-
+for (i in 1:12) 
+{
+  if (is.null(unlist(ss[sprintf("%02d",i)]))) v<-0
+    else v<-mean(ss[sprintf("%02d",i)][[1]]$tavg)
+  tavg<-c(tavg,v)
+}
 #i<-2
 
 tab1<-read.csv("tab1.csv")
@@ -61,7 +88,7 @@ f<-tab1$afi+tab1$bfi*s
 
 
 
-y<-10^6*(f*tab1$di*300/(1600*2.2*(100-25)))/10^7
+y<-10^6*sum(f*tab1$di*300/(1600*2.2*(100-25)))  
 y
             
             
